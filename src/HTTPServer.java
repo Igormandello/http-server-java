bba005f414bef416c41464d9eb8d0f970a514239
Igormandello <igormandello@gmail.com>
2018-08-13 11:57:58 -0300

    Iniciada a resposta HTTP
    - Ainda não está funcionando

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.File;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.security.InvalidParameterException;
+
+import sun.misc.IOUtils;
 
 public class HTTPServer extends Thread {
 	private int port;
@@ -31,8 +39,39 @@ public class HTTPServer extends Thread {
 				Socket client = socket.accept();
 				
 				BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				System.out.println("aceitou: " + reader.readLine());
+				OutputStreamWriter writer = new OutputStreamWriter(client.getOutputStream());
+				
+				String message = reader.readLine();
+				if (message != null && !message.equals("")) {
+					String[] data = message.split(" ");
+					if (data.length == 3 && data[0].equals("GET") && data[2].equals("HTTP/1.1")) {
+						String actualMessage = new String(message);
+						while (actualMessage != null && !actualMessage.equals("")) {
+							actualMessage = reader.readLine();
+							message += actualMessage;
+						}
+						
+						String fileRequested = URLDecoder.decode(data[1]);
+						System.out.println(fileRequested);
+						
+						writer.write("HTTP/1.1 200 OK");
+						writer.write("Connection: close");
+						writer.write("Date: Sat, 01 jan 2001 12:00:00 GMT");
+						writer.write("Server: Apache/1.3.0 (Unix)");
+						writer.write("Last-Modified: Sun, 6 May 2017 09:30:00 GMT");
+						writer.write("Content-Length: 6821");
+						writer.write("Content-Type: text/html");
+						URL url = getClass().getResource("../example/example.html");
+						File f = new File(url.getPath());
+						System.out.println(Files.readAllBytes(f.toPath()));
+						writer.flush();
+					}
+				} else {
+					writer.write("Invalid request");
+					writer.flush();
+				}
 			} catch (IOException e) {
+				System.out.println(e);
 				this.terminate();
 			}
 			
